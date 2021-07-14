0c7759a08b6799e7c80ae1c284ae189a1f1df036
sannuprajapati <78226381+sannuprajapati@users.noreply.github.com>
2021-07-14 13:39:12 +0530

    Update C_task.c
    new commit!

--- C_task.c
+++ C_task.c
@@ -3,7 +3,12 @@
 # include <stdio.h>
 main()
 {
-char ch;
+char ch ,g;
+
+
+int numberArray[10];
+int  i ,sum ,p ;
+
 FILE *fp;
 fp=fopen("f1.txt","r");
 
@@ -17,7 +22,33 @@ while(!feof(fp)){
 	printf("%c",ch);
 	ch=fgetc(fp);
 }
+
+/// finding total marks , percentage and grade
+
+for (i = 0; i < 5; i++){
+fscanf(fp, "%d,", &numberArray[i] );
+}
+
+for (i = 0; i < 5; i++)
+{sum=sum+(numberArray[i]);
+}
+
+p=(sum/5)*100;
+
+if ( p>80){g="A";}
+if (p<8 && p>35){g="B"; }
+else g="C";
+
+printf("Total Marks is: %d\n", sum);
+printf("Percentage =%d \n", p);  
+printf("Grade = %c\n", g);
+
 fclose(fp);
 
-return 0;
 }
+
+
+
+
+
+   
